2816193e72ee76629ea89a5f297a26e1d3a5e3a0
Ali <alifarooqipnb@gmail.com>
2025-07-05 18:02:46 +0500

    Bug Fix

--- src/App.jsx
+++ src/App.jsx
@@ -131,7 +131,7 @@ function Logo() {
 function NumResults({ movies }) {
   return (
     <p className="num-results">
-      Found <strong>X</strong> results
+      Found <strong>{movies.length}</strong> results
     </p>
   );
 }
